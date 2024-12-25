d9bd70d7e6f3e5dd333691b76739100ec117d73e
김동현 <69367698+indextrown@users.noreply.github.com>
2024-12-25 20:59:44 +0900

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by 김동현 on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
